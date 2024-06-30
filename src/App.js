4eadce15b33f63392e1be268ab9634bd6a4cbb61
Dwivedi <devweb910@gmail.com>
2024-06-30 13:12:29 +0530

    frontend connected with backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/msg")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="h-16 bg-green-500 text-gray-200">
+        <h1>{message}</h1>
+      </div>
+    </>
   );
 }
 
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 // const connectToMongo = require("./connection");
 
 const port = 5000;
 const server = express();
 
+server.use(cors());
+
+server.use(express.json())
+
+server.get("/msg", (req, res) => {
+  res.json({message : "Hello from server!"});
+});
+
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
