a5715e5abe1ae79cbbe8c5b6636c885f7f2b7798
Dwivedi <devweb910@gmail.com>
2024-07-01 12:43:53 +0530

    Login module completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./component/Login";
+import Error from "./component/Error";
+import Home from "./component/Home";
+import HomeContent from "./component/HomeContent";
+import Employee from "./component/admin/Employee";
+import AddEmployee from "./component/admin/AddEmployee";
+import ModifyEmployee from "./component/admin/ModifyEmployee";
+import RemoveEmployee from "./component/admin/RemoveEmployee";
+import Payroll from "./component/Payroll";
+import Summary from "./component/Summary";
+import ClientDashboard from "./component/client/ClientDashboard";
+import Profile from "./component/client/Profile";
+import Payslip from "./component/client/Payslip";
+import Information from "./component/client/Information";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:5000/msg")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
   return (
     <>
-      <div className="h-16 bg-green-500 text-gray-200">
-        <h1>{message}</h1>
-      </div>
+      <Router>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route path="/home" element={<Login /> ? <Home /> : <Error />}>
+            <Route path="" element={<HomeContent />} />
+            <Route path="employee" element={<AddEmployee />} />
+            <Route path="employee/all" element={<Employee />} />
+            <Route path="employee/modify" element={<ModifyEmployee />} />
+            <Route path="employee/remove" element={<RemoveEmployee />} />
+            <Route path="payroll" element={<Payroll />} />
+            <Route path="summary" element={<Summary />} />
+          </Route>
+          <Route path="/Dashboard" element={<ClientDashboard />}>
+            <Route path="" element={<Profile />} />
+            <Route path="information" element={<Information />} />
+            <Route path="payslip" element={<Payslip />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
-
 export default App;
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const cors = require("cors");
-// const connectToMongo = require("./connection");
+const bodyParser = require("body-parser");
+// const User = require("./modal/User");
+const connectToMongo = require("./connection");
 
 const port = 5000;
 const server = express();
-
 server.use(cors());
-
-server.use(express.json())
+server.use(bodyParser.json());
+connectToMongo();
 
 server.get("/msg", (req, res) => {
-  res.json({message : "Hello from server!"});
+  res.json({ message: "Hello from server!" });
+});
+
+server.post("/auth", async (req, res) => {
+  const userId = req.body.fname;
+  const userPassword = req.body.password;
+
+  if (userId === "admin" && userPassword === "admin") {
+    res.json({
+      status: true,
+      fname: "Admin",
+      user: "Admin",
+    });
+  } else {
+    // const user = await User.findOne({})
+    res.json({
+      status: true,
+      fname: "Employee",
+      user: "Employee",
+    });
+  }
 });
 
 server.listen(port, () => {
